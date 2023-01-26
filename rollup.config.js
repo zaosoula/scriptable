a5375d3a1b60e8bfe6fb504125cc6f534bb326f5
Zao Soula <contact@zaosoula.fr>
2023-01-26 12:25:13 +0100

    chore(build): add typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,46 @@
 /* eslint-disable */
 const commonjs = require('@rollup/plugin-commonjs');
 const glob = require('glob');
-const fs = require('fs');
 const { terser } = require('rollup-plugin-terser');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const typescript = require('@rollup/plugin-typescript');
 
-const input = glob.sync('src/*.js', {
+const scriptableIcloudPath = path.join(os.homedir(), 'Library/Mobile Documents/iCloud~dk~simonbs~Scriptable/Documents');
+
+const input = glob.sync('src/*.[tj]s', {
   absolute: true,
 });
 
+const keepScriptableVars = (input) => ({
+  generateBundle: (_, bundles) => Object.fromEntries(Object.entries(bundles).map(([name, bundle]) => {
+    console.log(input);
+    const entry = Object.keys(bundle.modules).find((x) => Array.isArray(input) ? input.includes(x) : input === x);
+    const code = fs.readFileSync(entry, { encoding: 'utf-8'});
+    let topComments = code.match(/(\/\/(.*)\n)+/m)[0];
+    topComments += `// Build at ${new Date().toISOString()}\n`;
+    bundle.code = topComments + bundle.code;
+    return [name, bundle];
+  })),
+});
+
+const cjsOutput = (dir) => ({
+  dir,
+  format: 'cjs',
+  plugins: [ keepScriptableVars(input) ],
+})
+
+
 module.exports = {
   input,
-  output: {
-    dir: 'dist',
-    format: 'cjs',
-    plugins: [
-      {
-        generateBundle: (_, bundles) => Object.fromEntries(Object.entries(bundles).map(([name, bundle]) => {
-          const entry = Object.keys(bundle.modules).find((x) => input.includes(x));
-          const topComments = bundle.modules[entry].code.match(/(\/\/(.*)\n)+/m)[0];
-          bundle.code = topComments + bundle.code;
-          return [name, bundle];
-        })),
-      },
-    ],
-  },
+  output: [
+    cjsOutput(scriptableIcloudPath),
+    cjsOutput('dist')
+  ],
   plugins: [
+    typescript(),
     terser(),
-    commonjs(),
+    // commonjs(),
   ],
 };
